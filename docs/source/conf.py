6bc3156897da79fd69b9f39c6affc830ee27e224
Daniel Mohns <daniel.mohns@posteo.de>
2024-02-01 11:10:01 +0100

    Update copyright year

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,12 +12,13 @@
 #
 import os
 import sys
+from datetime import datetime
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'MPManager'
-copyright = '2020, MPManager'
+copyright = f'{datetime.now().year}, MPManager'
 author = 'INENSUS IT Team'
 
 
